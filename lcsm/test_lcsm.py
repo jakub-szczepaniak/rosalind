79c5e83915962338ca5fb02a26fd1e1469532354
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2018-03-27 22:10:09 +0200

    extracting strands from dataset

--- lcsm/test_lcsm.py
+++ lcsm/test_lcsm.py
@@ -1,10 +1,23 @@
 import unittest
+from itertools import zip_longest
 
 class TestSharedMotif(unittest.TestCase):
-    def test_parsing_input(self):
+    def test_loading_dataset(self):
         dataset = load_dataset('rosalind_lcsm.txt')
-        print(dataset)
+        self.assertEqual(1800, len(dataset))
 
+    def test_merging_strands_from_dataset(self):
+        dataset = load_dataset('rosalind_lcsm.txt')
+        strands = parse_dataset(dataset)
+        self.assertEqual(100, len(strands))
+
+
+def extract_strands_from(dataset):
+    for i in range(0, len(dataset), 18):
+        yield dataset[i+1:i + 18]
+
+def parse_dataset(dataset):
+    return ["".join(strand) for strand in extract_strands_from(dataset)]
 
 def load_dataset(filename):
     all_lines = []
