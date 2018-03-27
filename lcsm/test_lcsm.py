f5d79a185744ed7a163ad768296aba1753bd9a42
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2018-03-27 21:40:37 +0200

    loading dataset

--- lcsm/test_lcsm.py
+++ lcsm/test_lcsm.py
@@ -1,12 +1,14 @@
 import unittest
 
 class TestSharedMotif(unittest.TestCase):
-  def test_parsing_input(self):
-    parsed_input = []
-    with open('rosalind_lcsm.txt', 'r') as dataset:
+    def test_parsing_input(self):
+        dataset = load_dataset('rosalind_lcsm.txt')
+        print(dataset)
+
+
+def load_dataset(filename):
+    all_lines = []
+    with open(filename, 'r') as dataset:
         for line in dataset.readlines():
-            parsed_input.append(line.rstrip())
-    print(parsed_input)
-
-
-
+            all_lines.append(line.rstrip())
+    return all_lines
