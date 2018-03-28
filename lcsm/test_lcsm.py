a02d305a8bd4bfe45e1f21eca1946cea220cfa04
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2018-03-29 00:05:44 +0200

    find common substring, fails

--- lcsm/test_lcsm.py
+++ lcsm/test_lcsm.py
@@ -1,7 +1,7 @@
 import unittest
 from itertools import zip_longest
 
-class TestSharedMotif(unittest.TestCase):
+class TestSharedMotifDatasetExtraction(unittest.TestCase):
     def test_loading_dataset(self):
         dataset = load_dataset('rosalind_lcsm.txt')
         self.assertEqual(1800, len(dataset))
@@ -11,7 +11,28 @@ class TestSharedMotif(unittest.TestCase):
         strands = parse_dataset(dataset)
         self.assertEqual(100, len(strands))
 
+class TestSharedPrepareStrandCandidates(unittest.TestCase):
+    def test_prepare_substrand_set(self):
+        sample_strand = 'abca'
+        result = {'b', 'c', 'ab', 'ca', 'abc', 'a', 'abca', 'bc', 'bca'}
 
+        self.assertEqual(result, prepare_set(sample_strand))
+    def test_for_sample_data(self):
+        dataset = load_dataset('rosalind_lcsm.txt')
+        strands = parse_dataset(dataset)
+        found = find_motif(strands)
+        print(found)
+        
+def find_motif(strands):
+    reference = sorted(prepare_set(strands[0]), reverse=True)
+    for motif in reference:
+        if all(motif in strand for strand in strands[1:]):
+            return motif
+    
+def prepare_set(input_string):
+  length = len(input_string)
+  return set([input_string[i:j+1] for i in range(length) for j in range(i, length)])
+        
 def extract_strands_from(dataset):
     for i in range(0, len(dataset), 18):
         yield dataset[i+1:i + 18]
