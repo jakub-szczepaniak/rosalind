e5ab3d0c2f7c16ca4b262d835af9a327d0944acf
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2018-03-31 20:47:18 +0200

    final submission

--- ini6/test_ini6.py
+++ ini6/test_ini6.py
@@ -2,8 +2,8 @@ import unittest
 
 class TestIni6(unittest.TestCase):
     def test_solving_the_dict(self):
-        sentence = 'We tried list and we tried dicts also we tried Zen'
-        output = dict(default=0)
+        sentence = 'When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom let it be And in my hour of darkness she is standing right in front of me Speaking words of wisdom let it be Let it be let it be let it be let it be Whisper words of wisdom let it be And when the broken hearted people living in the world agree There will be an answer let it be For though they may be parted there is still a chance that they will see There will be an answer let it be Let it be let it be let it be let it be There will be an answer let it be Let it be let it be let it be let it be Whisper words of wisdom let it be Let it be let it be let it be let it be Whisper words of wisdom let it be And when the night is cloudy there is still a light that shines on me Shine until tomorrow let it be I wake up to the sound of music Mother Mary comes to me Speaking words of wisdom let it be Let it be let it be let it be yeah let it be There will be an answer let it be Let it be let it be let it be yeah let it be Whisper words of wisdom let it be'
+        output = {}
         for word in sentence.split(' '):
             if word not in output.keys():
                 output[word] = 1
