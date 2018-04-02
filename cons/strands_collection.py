5af1b8fec26549cf06b801ecd0d811540fc06138
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2018-04-02 20:32:20 +0200

    we now validate the input for Strands Collection

--- cons/strands_collection.py
+++ cons/strands_collection.py
@@ -1,3 +1,7 @@
 class StrandsCollection:
-    def __init__(self):
-        pass
+    def __init__(self, list_of_strands):
+        if len(list_of_strands) == 0:
+            raise CollectionNotValid('Cannot be empty!')
+
+class CollectionNotValid(Exception):
+    pass
--- cons/test_cons.py
+++ cons/test_cons.py
@@ -1,5 +1,6 @@
 import unittest
-from strands_collection import StrandsCollection
+from strands_collection import StrandsCollection, CollectionNotValid
 class TestStrandsConsensusAndProfile(unittest.TestCase):
-    def test_collection_of_strands_can_be_created(self):
-        self.assertEquals(StrandsCollection(), None)
+    def test_collection_of_strands_takes_list_of_items(self):
+        with self.assertRaises(CollectionNotValid):
+            StrandsCollection([])
