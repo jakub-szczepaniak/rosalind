a5dfc9537fe1530de56ca96573d6d71a6e038414
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2018-04-02 21:07:11 +0200

    scaffold for Strands Collection class

--- cons/strands_collection.py
+++ cons/strands_collection.py
@@ -2,6 +2,12 @@ class StrandsCollection:
     def __init__(self, list_of_strands):
         if len(list_of_strands) == 0:
             raise CollectionNotValid('Cannot be empty!')
+        self.strands = list_of_strands
+        self._consensus = self.strands[0]
+
+    @property
+    def consensus(self):
+        return self._consensus
 
 class CollectionNotValid(Exception):
     pass
--- cons/test_cons.py
+++ cons/test_cons.py
@@ -4,3 +4,10 @@ class TestStrandsConsensusAndProfile(unittest.TestCase):
     def test_collection_of_strands_takes_list_of_items(self):
         with self.assertRaises(CollectionNotValid):
             StrandsCollection([])
+    def test_collection_stores_strands(self):
+        sample = ['123', '123']
+        self.assertEqual(StrandsCollection(sample).strands, sample)
+
+    def test_profile_is_equal_to_when_single_strands(self):
+        sample = StrandsCollection(['AGCT'])
+        self.assertEqual(sample.consensus, 'AGCT')
