41096906979fdf46f506fa35817dd07ea5067f0c
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2018-04-02 21:34:59 +0200

    moved validation to separate method

--- cons/strands_collection.py
+++ cons/strands_collection.py
@@ -1,14 +1,19 @@
 class StrandsCollection:
     def __init__(self, list_of_strands):
+        self.validate(list_of_strands)
+        length = len(list_of_strands[0])
+
+        self.strands = list_of_strands
+        self._consensus = self.strands[0]
+        
+        self._profile = {'A':[0]*length, 'C': [0]*length, 'G':[0]*length, 'T':[0]*length}
+
+    def validate(self, list_of_strands):
         if len(list_of_strands) == 0:
             raise CollectionNotValid('Cannot be empty!')
         length = len(list_of_strands[0])
         if any(len(strand) != length for strand in list_of_strands):
             raise CollectionNotValid('Strands have to have same length!')
-        self.strands = list_of_strands
-        self._consensus = self.strands[0]
-        
-        self._profile = {'A':[0]*length, 'C': [0]*length, 'G':[0]*length, 'T':[0]*length}
 
     @property
     def consensus(self):
